4888aec1ea9df7a5389fed3932c8e9c35aad4d5b
Molnár Márton <molnar.marton@gmail.com>
2020-11-12 09:24:24 +0100

    egyházmegyék és városok (model & admin)

--- zsolozsma/admin.py
+++ zsolozsma/admin.py
@@ -4,25 +4,51 @@ import zsolozsma.models
 
 class EventScheduleInline(admin.TabularInline):
     model = zsolozsma.models.EventSchedule
-    readonly_fields = ('hash',)
+    readonly_fields = ('hash', )
 
 
 @admin.register(zsolozsma.models.Event)
 class EventAdmin(admin.ModelAdmin):
     model = zsolozsma.models.Event
-    inlines = [EventScheduleInline, ]
-    ordering = ['location', 'liturgy']
+    inlines = [
+        EventScheduleInline,
+    ]
+    ordering = ['location__name', 'liturgy']
+    list_display = ('location', 'liturgy')
 
 
 class EventInline(admin.TabularInline):
     model = zsolozsma.models.Event
 
 
+@admin.register(zsolozsma.models.City)
+class CityAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug', )
+    ordering = ['name']
+    list_display = ('name', 'diocese')
+
+
+class CityInline(admin.TabularInline):
+    model = zsolozsma.models.City
+
+
+@admin.register(zsolozsma.models.Diocese)
+class DioceseAdmin(admin.ModelAdmin):
+    readonly_fields = ('slug', )
+    inlines = [
+        CityInline,
+    ]
+    ordering = ['name']
+
+
 @admin.register(zsolozsma.models.Location)
 class LocationAdmin(admin.ModelAdmin):
-    readonly_fields = ('slug',)
-    inlines = [EventInline, ]
+    readonly_fields = ('slug', )
+    inlines = [
+        EventInline,
+    ]
     ordering = ['name']
+    list_display = ('name', 'city')
 
 
 class LiturgyTextInline(admin.TabularInline):
@@ -31,8 +57,10 @@ class LiturgyTextInline(admin.TabularInline):
 
 @admin.register(zsolozsma.models.Liturgy)
 class LiturgyAdmin(admin.ModelAdmin):
-    readonly_fields = ('slug',)
-    inlines = [LiturgyTextInline, ]
+    readonly_fields = ('slug', )
+    inlines = [
+        LiturgyTextInline,
+    ]
     ordering = ['name']
 
 
--- zsolozsma/models.py
+++ zsolozsma/models.py
@@ -4,19 +4,75 @@ from django.utils.text import slugify
 import secrets
 
 
+class Diocese(models.Model):
+    """Egyházmegye"""
+
+    name = models.CharField('Név', blank=False, unique=True, max_length=200)
+    slug = models.SlugField('URL részlet',
+                            max_length=100,
+                            blank=False,
+                            unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Diocese, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Egyházmegye'
+        verbose_name_plural = 'Egyházmegyék'
+
+
+class City(models.Model):
+    """Település"""
+
+    name = models.CharField('Név', blank=False, unique=True, max_length=50)
+    slug = models.SlugField('URL részlet',
+                            max_length=100,
+                            blank=False,
+                            unique=True)
+    diocese = models.ForeignKey("Diocese",
+                                verbose_name='Egyházmegye',
+                                on_delete=models.CASCADE,
+                                blank=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(City, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Település'
+        verbose_name_plural = 'Települések'
+
+
 class Location(models.Model):
     """Helyszín"""
 
     name = models.CharField('Rövid név', max_length=100, blank=False)
-    fullname = models.CharField(
-        'Teljes név', max_length=300, blank=False, unique=True)
-    slug = models.SlugField('URL részlet', max_length=50,
-                            blank=False, unique=True)
+    fullname = models.CharField('Teljes név',
+                                max_length=300,
+                                blank=False,
+                                unique=True)
+    slug = models.SlugField('URL részlet',
+                            max_length=50,
+                            blank=False,
+                            unique=True)
+    city = models.ForeignKey("City",
+                             verbose_name='Település',
+                             on_delete=models.CASCADE,
+                             null=True)
     homepage = models.URLField('Honlap', blank=True)
-    youtube_channel = models.CharField(
-        'YouTube csatorna ID', max_length=24, blank=True)
-    video_url = models.URLField(
-        'URL a közvetítéshez', max_length=500, blank=True)
+    youtube_channel = models.CharField('YouTube csatorna ID',
+                                       max_length=24,
+                                       blank=True)
+    video_url = models.URLField('URL a közvetítéshez',
+                                max_length=500,
+                                blank=True)
     is_active = models.BooleanField('Aktív', blank=False, default=True)
 
     def save(self, *args, **kwargs):
@@ -36,8 +92,10 @@ class Liturgy(models.Model):
 
     name = models.CharField('Név', max_length=100, blank=False, unique=True)
     description = models.TextField('Leírás', max_length=500, blank=True)
-    slug = models.SlugField('URL részlet', max_length=50,
-                            blank=False, unique=True)
+    slug = models.SlugField('URL részlet',
+                            max_length=50,
+                            blank=False,
+                            unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -55,16 +113,22 @@ class Event(models.Model):
     """Közvetített esemény (adott szertartás egy adott helyen)"""
 
     name = models.CharField('Név', max_length=100, blank=False)
-    location = models.ForeignKey(
-        "Location", verbose_name='Helyszín', on_delete=models.CASCADE, blank=False)
-    liturgy = models.ForeignKey(
-        "Liturgy", verbose_name='Szertartás', on_delete=models.CASCADE, blank=False)
+    location = models.ForeignKey("Location",
+                                 verbose_name='Helyszín',
+                                 on_delete=models.CASCADE,
+                                 blank=False)
+    liturgy = models.ForeignKey("Liturgy",
+                                verbose_name='Szertartás',
+                                on_delete=models.CASCADE,
+                                blank=False)
     duration = models.IntegerField('Időtartam (perc)', blank=True, null=True)
 
-    youtube_channel = models.CharField(
-        'Egyedi YouTube csatorna ID', max_length=24, blank=True)
-    video_url = models.URLField(
-        'Egyedi URL a közvetítéshez', max_length=500, blank=True)
+    youtube_channel = models.CharField('Egyedi YouTube csatorna ID',
+                                       max_length=24,
+                                       blank=True)
+    video_url = models.URLField('Egyedi URL a közvetítéshez',
+                                max_length=500,
+                                blank=True)
     text_url = models.URLField('Egyedi szöveg URL', max_length=500, blank=True)
 
     is_active = models.BooleanField('Aktív', blank=False, default=True)
@@ -78,8 +142,9 @@ class Event(models.Model):
 
 
 class EventSchedule(models.Model):
-    event = models.ForeignKey(
-        "Event", verbose_name="Esemény", on_delete=models.CASCADE)
+    event = models.ForeignKey("Event",
+                              verbose_name="Esemény",
+                              on_delete=models.CASCADE)
 
     hash = models.CharField('URL hash', max_length=8, unique=True, null=True)
 
@@ -92,15 +157,22 @@ class EventSchedule(models.Model):
         szombat = 5,
         vasárnap = 6
 
-    day_of_week = models.IntegerField(
-        'Hét napja', null=True, blank=True, choices=Weekdays.choices)
-    time = models.TimeField('Kezdés ideje', auto_now=False,
-                            auto_now_add=False, null=True, blank=True)
+    day_of_week = models.IntegerField('Hét napja',
+                                      null=True,
+                                      blank=True,
+                                      choices=Weekdays.choices)
+    time = models.TimeField('Kezdés ideje',
+                            auto_now=False,
+                            auto_now_add=False,
+                            null=True,
+                            blank=True)
 
-    youtube_channel = models.CharField(
-        'Egyedi YouTube csatorna ID', max_length=24, blank=True)
-    video_url = models.URLField(
-        'Egyedi URL a közvetítéshez', max_length=500, blank=True)
+    youtube_channel = models.CharField('Egyedi YouTube csatorna ID',
+                                       max_length=24,
+                                       blank=True)
+    video_url = models.URLField('Egyedi URL a közvetítéshez',
+                                max_length=500,
+                                blank=True)
     text_url = models.URLField('Egyedi szöveg URL', max_length=500, blank=True)
 
     def __str__(self):
@@ -109,7 +181,7 @@ class EventSchedule(models.Model):
         return "%s (%s %s)" % (self.event.name, date_str, self.time)
 
     def save(self, *args, **kwargs):
-        if(self.pk is None or self.hash is None):
+        if (self.pk is None or self.hash is None):
             self.hash = secrets.token_hex(4)
 
         super(EventSchedule, self).save(*args, **kwargs)
@@ -122,10 +194,14 @@ class EventSchedule(models.Model):
 class LiturgyText(models.Model):
     """Egy szertartás szövegei"""
 
-    liturgy = models.ForeignKey(
-        "Liturgy", verbose_name="Szertartás", on_delete=models.CASCADE, blank=False)
-    date = models.DateField("Dátum", auto_now=False,
-                            auto_now_add=False, blank=False)
+    liturgy = models.ForeignKey("Liturgy",
+                                verbose_name="Szertartás",
+                                on_delete=models.CASCADE,
+                                blank=False)
+    date = models.DateField("Dátum",
+                            auto_now=False,
+                            auto_now_add=False,
+                            blank=False)
     text_url = models.URLField('Szöveg URL', max_length=500, blank=True)
 
     class Meta:
@@ -134,18 +210,25 @@ class LiturgyText(models.Model):
 
 
 class Broadcast(models.Model):
-    schedule = models.ForeignKey(
-        "EventSchedule", verbose_name='Esemény időpont', null=True, on_delete=models.CASCADE)
-    date = models.DateField('Dátum', auto_now=False,
-                            auto_now_add=False, blank=False)
+    schedule = models.ForeignKey("EventSchedule",
+                                 verbose_name='Esemény időpont',
+                                 null=True,
+                                 on_delete=models.CASCADE)
+    date = models.DateField('Dátum',
+                            auto_now=False,
+                            auto_now_add=False,
+                            blank=False)
     video_url = models.URLField('Videó URL', max_length=500, blank=False)
-    video_iframe = models.BooleanField(
-        'Videó beágyazható', blank=True, default=True)
-    video_only = models.BooleanField(
-        'Beágyazás csak maga a videó', blank=True, default=True)
+    video_iframe = models.BooleanField('Videó beágyazható',
+                                       blank=True,
+                                       default=True)
+    video_only = models.BooleanField('Beágyazás csak maga a videó',
+                                     blank=True,
+                                     default=True)
     text_url = models.URLField('Szöveg URL', max_length=500, blank=True)
-    text_iframe = models.BooleanField(
-        'Szöveg beágyazható', blank=True, default=True)
+    text_iframe = models.BooleanField('Szöveg beágyazható',
+                                      blank=True,
+                                      default=True)
 
     class Meta:
         verbose_name = 'Közvetítés'
@@ -153,4 +236,5 @@ class Broadcast(models.Model):
 
     def __str__(self):
         event = self.schedule.event
-        return "%s %s %s %s" % (event.location.name, event.name, self.date, self.schedule.time)
+        return "%s %s %s %s" % (event.location.name, event.name, self.date,
+                                self.schedule.time)
