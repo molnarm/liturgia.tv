b0ed3684c2933955cd1179ef9461c1a8c71ca9fb
Molnár Márton <molnar.marton@gmail.com>
2020-11-13 08:05:13 +0100

    logging

--- website/settings.py
+++ website/settings.py
@@ -122,6 +122,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
 
 # Activate Django-Heroku.
 django_heroku.settings(locals(), logging=not DEBUG, databases=not DEBUG)
