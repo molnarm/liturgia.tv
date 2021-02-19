df1dee36365f729bd541f1acd43d9331c10038b2
Molnár Márton <molnar.marton@gmail.com>
2021-02-19 16:57:44 +0100

    addig is működjön a miserend.hu

--- zsolozsma/middleware.py
+++ zsolozsma/middleware.py
@@ -6,7 +6,7 @@ class RedirectToCustomDomainMiddleware(MiddlewareMixin):
     def process_request(self, request):
         domain = 'liturgia.tv'
         host = request.get_host()
-        if (domain not in host):
+        if ((domain not in host) and ('json' not in request.GET)):
             redirect_url = '%s://%s%s' % (request.scheme, domain,
                                           request.get_full_path())
             return HttpResponsePermanentRedirect(redirect_url)
