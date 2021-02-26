d22860116de44795c4a3420b2afe63b74cf84c7e
Molnár Márton <molnar.marton@gmail.com>
2021-02-26 16:46:19 +0100

    #29

--- zsolozsma/youtube.py
+++ zsolozsma/youtube.py
@@ -1,6 +1,5 @@
 def get_embed(channel):
-
-    return 'https://www.youtube.com/embed/live_stream?channel=' + channel
+    return 'https://www.youtube.com/embed/live_stream?channel=' + channel + '&autoplay=1'
 
 
 def get_link(channel):
