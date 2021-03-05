6943c8a00dcdd234f707044393eb596b2cbfc010
Molnár Márton <molnar.marton@gmail.com>
2021-03-05 19:00:31 +0100

    érthetőbb admin táblázatok

--- zsolozsma/admin.py
+++ zsolozsma/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 import zsolozsma.models
 from django.utils import timezone
+from django.db.models.functions import Concat
 
 
 class EventScheduleInline(admin.TabularInline):
@@ -14,8 +15,24 @@ class EventAdmin(admin.ModelAdmin):
     inlines = [
         EventScheduleInline,
     ]
-    ordering = ['location__name', 'liturgy']
-    list_display = ('location', 'liturgy')
+
+    def location_name(self, event):
+        return event.location.city.name + ', ' + event.location.name
+
+    location_name.short_description = 'Helyszín'
+    location_name.admin_order_field = Concat('location__city__name',
+                                             'location__name')
+
+    def liturgy_name(self, event):
+        return event.liturgy.name
+
+    liturgy_name.short_description = 'Szertartás'
+    liturgy_name.admin_order_field = 'liturgy__name'
+
+    list_display = ('pk', 'location_name', 'liturgy_name')
+    list_display_links = ('pk', )
+    ordering = ['location__city__name', 'location__name', 'liturgy__name']
+    list_filter = ('liturgy__denomination', 'location__city')
 
 
 class EventInline(admin.TabularInline):
@@ -34,8 +51,17 @@ class LocationAdmin(admin.ModelAdmin):
     inlines = [
         EventInline,
     ]
-    ordering = ['name']
-    list_display = ('name', 'city')
+
+    def city_name(self, location):
+        return location.city.name
+
+    city_name.short_description = 'Település'
+    city_name.admin_order_field = 'city__name'
+
+    list_display = ('city_name', 'name')
+    list_display_links = ('name', )
+    ordering = ['city__name', 'name']
+    list_filter = ('city', )
 
 
 class LiturgyTextInline(admin.TabularInline):
@@ -54,8 +80,17 @@ class LiturgyAdmin(admin.ModelAdmin):
     inlines = [
         LiturgyTextInline,
     ]
-    ordering = ['name']
-    list_display = ('name', 'denomination')
+
+    def denomination_name(self, liturgy):
+        return liturgy.denomination.name
+
+    denomination_name.short_description = 'Felekezet/rítus'
+    denomination_name.admin_order_field = 'denomination__name'
+
+    list_display = ('denomination_name', 'name')
+    list_display_links = ('name', )
+    ordering = ['denomination__name', 'name']
+    list_filter = ('denomination', )
 
 
 class LiturgyInline(admin.TabularInline):
@@ -73,4 +108,34 @@ class DenominationAdmin(admin.ModelAdmin):
 
 @admin.register(zsolozsma.models.Broadcast)
 class BroadcastAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('schedule', 'date')
+
+    def location_name(self, broadcast):
+        return broadcast.schedule.event.location.city.name + ', ' + broadcast.schedule.event.location.name
+
+    location_name.short_description = 'Helyszín'
+    location_name.admin_order_field = Concat(
+        'schedule__event__location__city__name',
+        'schedule__event__location__name')
+
+    def liturgy_name(self, broadcast):
+        return broadcast.schedule.event.liturgy.name
+
+    liturgy_name.short_description = 'Szertartás'
+    liturgy_name.admin_order_field = 'schedule__event__liturgy__name'
+
+    def datetime(self, broadcast):
+        return str(broadcast.date) + ' ' + str(broadcast.schedule.time)
+
+    datetime.short_description = 'Időpont'
+    datetime.admin_order_field = Concat('date', 'schedule__time')
+
+    def hash(self, broadcast):
+        return broadcast.schedule.hash
+
+    list_display = ('hash', 'datetime', 'location_name', 'liturgy_name')
+    list_display_links = ('datetime', )
+    ordering = [
+        'date', 'schedule__time', 'schedule__event__location__city__name',
+        'schedule__event__location__name'
+    ]
